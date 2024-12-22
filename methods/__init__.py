4629e460710b1aafa28f401bdc2a6e61f5cd051b
z1358 <45870160+z1358@users.noreply.github.com>
2024-12-22 20:42:48 +0800

    Add files via upload

---
+++ methods/__init__.py
@@ -0,0 +1,12 @@
+from methods.source import Source
+from methods.norm import BNTest, BNAlpha, BNEMA
+from methods.cotta import CoTTA
+from methods.rmt import RMT
+from methods.rotta import RoTTA
+from methods.tent import Tent
+from methods.santa import SANTA
+from methods.obao import OBAO
+
+__all__ = [
+    'Source', 'BNTest', 'BNAlpha', 'BNEMA', 'CoTTA', 'RMT', 'SANTA', 'RoTTA', 'Tent', 'OBAO'
+]
